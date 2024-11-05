412e0e140f53318cca351cace73b90c4dcc0d820
Heman Ho <heman.ho.lh@gmail.com>
2024-11-05 13:29:36 -0800

    Fixed login to home page, user will stay logged in after closing app

--- app/src/main/java/ca/sfu/memoryhub/Login.java
+++ app/src/main/java/ca/sfu/memoryhub/Login.java
@@ -27,58 +27,61 @@ public class Login extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    setContentView(R.layout.login);
-    mAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.login);
+        mAuth = FirebaseAuth.getInstance();
 
-    //Find buttons and edit text
-    btnBackToStart = findViewById(R.id.btnBackToStart2);
-    btnLogin = findViewById(R.id.btnLoginToApp);
-    edtEmailLog = findViewById(R.id.edtEmailAddressLog);
-    edtPasswordLog = findViewById(R.id.edtPasswordLog);
+        //Find buttons and edit text
+        btnBackToStart = findViewById(R.id.btnBackToStart2);
+        btnLogin = findViewById(R.id.btnLoginToApp);
+        edtEmailLog = findViewById(R.id.edtEmailAddressLog);
+        edtPasswordLog = findViewById(R.id.edtPasswordLog);
 
-    btnBackToStart.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent i = new Intent(Login.this, StartPage.class);
-            startActivity(i);
-        }
-    });
+        btnBackToStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, StartPage.class);
+                startActivity(i);
+            }
+        });
 
-    btnLogin.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            //Get what was inputted into email and password fields
-            String strEmail = edtEmailLog.getText().toString();
-            String strPassword = edtPasswordLog.getText().toString();
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Get what was inputted into email and password fields
+                String strEmail = edtEmailLog.getText().toString();
+                String strPassword = edtPasswordLog.getText().toString();
 
-            //Don't do anything if there is an invalid input
-            if(strEmail.isEmpty() || strPassword.isEmpty()){
-                return;
+                //Don't do anything if there is an invalid input
+                if(strEmail.isEmpty() || strPassword.isEmpty()){
+                    return;
+                }
+
+                //Authenticate login credentials
+                mAuth.signInWithEmailAndPassword(strEmail, strPassword)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // If sign in is successful, display message
+                                    Toast.makeText(Login.this, "Authentication Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(Login.this, MainActivity.class);
+                                    startActivity(i);
+                                    finish();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(Login.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+
+
             }
 
-            //Authenticate login credentials
-            mAuth.signInWithEmailAndPassword(strEmail, strPassword)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                // If sign in is successful, display message
-                                Toast.makeText(Login.this, "Authentication Successful.",
-                                        Toast.LENGTH_SHORT).show();
-
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(Login.this, "Authentication failed.",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        }
-                    });
-
-
-
-        }
-
-    });
+        });
 
 
     }
